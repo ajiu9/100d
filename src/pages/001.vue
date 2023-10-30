2864d2cd67aea8d2ef0db626be24be8c47229238
ajiu9 <6159443232@qq.com>
2023-10-30 22:07:59 +0800

    chore: plum

---
+++ src/pages/001.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { useRafFn } from 'commonuse'
+import { onMounted, ref, watch } from 'vue'
+import { initCanvas, polar2cart, r15, r180, r90 } from '../utils'
+
+const el = ref<HTMLCanvasElement | null>(null)
+const { random } = Math
+const f = {
+  start: () => {},
+}
+
+const init = ref(5)
+const len = ref(5)
+const stopped = ref(false)
+
+watch([init, len], () => f.start())
+
+onMounted(() => {
+  const canvas = el.value!
+  const { ctx } = initCanvas(canvas)
+  const { width, height } = canvas
+
+  let steps: Function[] = []
+  let prevSteps: Function[] = []
+
+  let iterations = 0
+  const step = (x: number, y: number, rad: number) => {
+    const length = random() * len.value
+
+    const [nx, ny] = polar2cart(x, y, length, rad)
+
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(nx, ny)
+    ctx.stroke()
+
+    const rad1 = rad + random() * r15
+    const rad2 = rad - random() * r15
+
+    if (nx < -100 || nx > 500 || ny < -100 || ny > 500)
+      return
+
+    if (iterations <= init.value || random() > 0.5)
+      steps.push(() => step(nx, ny, rad1))
+    if (iterations <= init.value || random() > 0.5)
+      steps.push(() => step(nx, ny, rad2))
+  }
+  const frame = () => {
+    iterations += 1
+    prevSteps = steps
+    steps = []
+
+    if (!prevSteps.length) {
+      controls.pause()
+      stopped.value = true
+    }
+    prevSteps.forEach(i => i())
+  }
+
+  const controls = useRafFn(frame, { immediate: false })
+
+  f.start = () => {
+    controls.pause()
+    iterations = 0
+    ctx.clearRect(0, 0, width, height)
+    ctx.lineWidth = 1
+    ctx.strokeStyle = '#00000040'
+    prevSteps = []
+    steps = random() < 0.5
+      ? [
+          () => step(0, random() * 400, 0),
+          () => step(400, random() * 400, r180),
+        ]
+      : [
+          () => step(random() * 400, 0, r90),
+          () => step(random() * 400, 400, -r90),
+        ]
+    controls.resume()
+    stopped.value = false
+  }
+
+  f.start()
+})
+</script>
+
+<template>
+  <canvas ref="el" width="400" height="400" />
+</template>
